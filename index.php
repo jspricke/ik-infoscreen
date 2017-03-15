1f23d78a42fe57bda9f972eaed36aaf425b9e2f0
Sebastian <shoeffner@guenne.ik>
2017-03-16 00:55:33 +0100

    image layout adjustments

--- index.php
+++ index.php
@@ -85,14 +85,14 @@ function event_group_list_item($event, $start_time, $evening_time, $instructors)
     $instructor = $instructors[$abbr];
 
     echo sprintf('<a href="./details/detail%s.html" class="list-group-item">' .
-                    '<div>%s<span class="label" style="background-color:%s;">%s</span> <strong>%s</strong><span class="badge pull-right">%s</span></div>' .
+                    '<div>%s<span class="label" style="background-color:%s;">%s</span> <strong>%s</strong><span class="badge pull-right">%s</span></div><div style="clear:both;"></div>' .
                     '<div><small>%s</small></div>' .
                     '</a>',
                  $id, $time, substr($color, 0, 7), $abbr, $instructor, $location, $title);
 }
 
 function fluid_if_fullscreen() {
-    return !isset($_GET['fullscreen']) ? 'fluid-container' : 'container';
+    return !isset($_GET['fullscreen']) ? 'container-fluid' : 'container';
 }
 
 $chat = read_shouts();
@@ -100,103 +100,6 @@ $schedule_json = read_schedule();
 $schedule = filter_schedule($schedule_json, $TODAY);
 $schedule_copy = $schedule;
 
-?><!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Today's IK Schedule</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="./js/jquery-3.1.1.min.js"></script>
-        <script src="./js/bootstrap-3.3.7.min.js"></script>
-        <link href="./css/bootstrap-3.3.7.min.css" rel="stylesheet">
-        <script type="text/javascript" src="./js/coolclock.js"></script>
-        <script type="text/javascript" src="./js/ikterminal.js"></script>
-        <script type="text/javascript">
-            const loader = function() {
-                CoolClock.findAndCreateClocks();
-                refreshBySchedule('shoutbox', 5);
-                refreshBySchedule('impression', 20);
-                document.getElementById('shoutboxmessage').addEventListener('onkeydown', sendShoutbox);
-                document.getElementById('shoutboxform').addEventListener('submit', submitShoutbox);
-            };
-        </script>
-        <style type="text/css">
-        </style>
-    </head>
-    <body onload="loader();">
-        <div class="<?= fluid_if_fullscreen(); ?>">
-            <div class="page-header">
-                <h1>IK <?php echo $start_date->format('Y'); ?> <small>Day <?php echo $current_day; ?></small><small class="pull-right"><a href="http://guenne.ik">http://guenne.ik</a></small></h1>
-            </div>
-
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-9">
-                <div class="fluid-container">
-                        <div class="row hidden-md hidden-lg">
-                            <?php foreach ($TIMES as $start_time => $end_time) : ?>
-                            <div class="col-xs-12 col-sm-12">
-                                <div class="list-group">
-                                    <a href="" class="list-group-item active"><strong><?= $start_time ?> &ndash; <?= $end_time ?></strong></a>
-                                    <?php foreach ($schedule as $event) { event_group_list_item($event, $start_time, $evening_date, $INSTRUCTORS); } ?>
-                                </div>
-                            </div>
-                            <?php endforeach ?>
-                        </div>
-                        <div class="row hidden-sx hidden-sm">
-                            <?php foreach ($TIMES as $start_time => $end_time) : ?>
-                            <div class="col-md-2 col-lg-2 <?php if ($start_time == '09:00') { echo 'col-md-offset-1 col-lg-offset-1'; } ?>">
-                                <div class="list-group">
-                                    <a href="" class="list-group-item active"><strong><?= $start_time ?> &ndash; <?= $end_time ?></strong></a>
-                                    <?php foreach ($schedule as $event) { event_group_list_item($event, $start_time, $evening_date, $INSTRUCTORS); } ?>
-                                </div>
-                            </div>
-                            <?php endforeach ?>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-xs-12 col-sm-12 col-md-3 panel panel-default">
-                        <div class="panel-heading hidden-xs hidden-sm">Shoutbox</div>
-                        <form action="shoutbox.php" method="post" id="shoutboxform">
-                            <input type="text" id="shoutboxmessage" name="msg" accesskey="s" placeholder="message" style="width:100%;">
-                        </form>
-                        <div class="fluid-container" style="height: 13em; overflow-y: auto;">
-                            <table id="shoutbox" class="table table-condensed">
-                            <?php foreach ($chat as $line) : list($time, $ip, $msg) = explode(' ', $line, 3) ?>
-                                <tr>
-                                    <td width="20" bgcolor="<?= substr(md5($ip), 0, 6); ?>"></td>
-                                    <td><span class="text-muted"><?= date('d. H:i', $time) ?></span> <?= trim($msg); ?></td>
-                                </tr>
-                            <?php endforeach ?>
-                            </table>
-                        </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="<?= fluid_if_fullscreen(); ?>">
-            <div class="row well">
-                <!-- link box -->
-                <div class="col-xs-12 col-sm-12 col-md-4">
-                    <div class="text-center">
-                        <ul class="list-inline">
-                            <li><a href="http://guenne.ik/slides">http://guenne.ik/slides</a></li>
-                            <li><a href="http://guenne.ik/images">http://guenne.ik/images</a></li>
-                            <li><a href="https://www.facebook.com/groups/270641113015786/?fref=nf">IK Facebook Group</a></li>
-                            <li>Please upload your slides here: <a href="http://guenne.ik/incoming">http://guenne.ik/incoming</a></li>
-                        </ul>
-                    </div>
-                    <div class="panel">
-                        <div class="panel-header"><h4>Announcements</h4></div>
-                        <div class="panel-body">
-                            <p>Do you have images from IK? Please share with us. <br>&ndash; Jochen and Michael</p>
-                            <p>Hate paywalls? Paste the URL into <a href="http://sci-hub.io">sci-hub.io</a>.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- picture box -->
-<?php
 $path = 'thumbs/';
 
 function getImagesFromDir($path) {
@@ -223,23 +126,141 @@ function getRandomFromArray($ar) {
 $imgList = getImagesFromDir($path);
 
 $img = getRandomFromArray($imgList);
-?>
-                <div class="col-sm-12 col-md-4 hidden-xs text-center">
-                    <a href="#" class="thumbnail">
-                        <img src="<?php echo $path . $img ?>" alt="IK Impression" id="impression">
-                    </a>
-                </div>
+?><!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Today's IK Schedule</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="./js/jquery-3.1.1.min.js"></script>
+        <script src="./js/bootstrap-3.3.7.min.js"></script>
+        <link href="./css/bootstrap-3.3.7.min.css" rel="stylesheet">
+        <script type="text/javascript" src="./js/coolclock.js"></script>
+        <script type="text/javascript" src="./js/ikterminal.js"></script>
+        <script type="text/javascript">
+            const loader = function() {
+                CoolClock.findAndCreateClocks();
+                refreshBySchedule('shoutbox', 5);
+                refreshBySchedule('impression', 20);
+                document.getElementById('shoutboxmessage').addEventListener('onkeydown', sendShoutbox);
+                document.getElementById('shoutboxform').addEventListener('submit', submitShoutbox);
+            };
+        </script>
+        <style type="text/css">
+		.col-xs-5ths,
+		.col-sm-5ths,
+		.col-md-5ths,
+		.col-lg-5ths {
+		    position: relative;
+		    min-height: 1px;
+		    padding-right: 10px;
+		    padding-left: 10px;
+                    font-size:120%;
+		}
 
-                 <!-- clock box -->
-                <div class="col-md-4 hidden-sm hidden-xs text-center">
-                    <canvas id="clockid" class="CoolClock:noSeconds" width="170" height="170" style="width: 170px; height: 170px;"></canvas>
-                </div>
+		.col-xs-5ths {
+		    width: 20%;
+		    float: left;
+		}
 
-                <!-- other -->
-                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+		@media (min-width: 768px) {
+		    .col-sm-5ths {
+			width: 20%;
+			float: left;
+		    }
+		}
+
+		@media (min-width: 992px) {
+		    .col-md-5ths {
+			width: 20%;
+			float: left;
+		    }
+		}
+
+		@media (min-width: 1200px) {
+		    .col-lg-5ths {
+			width: 20%;
+			float: left;
+		    }
+		}
+        </style>
+    </head>
+    <body onload="loader();">
+        <div class="<?= fluid_if_fullscreen(); ?>">
+            <div class="page-header">
+		<h1>IK <?php echo $start_date->format('Y'); ?> <small>Day <?php echo $current_day; ?></small> <span class="hidden-xs hidden-sm pull-right"><small><a href="http://guenne.ik">http://guenne.ik</a></small> <span id="time"></span></span></h1>
             </div>
 
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-9" style="padding:0;">
+                <div class="container-fluid">
+		    <?php foreach ($TIMES as $start_time => $end_time) : ?>
+		    <div class="col-xs-12 col-sm-12 col-md-5ths col-lg-5ths">
+			<div class="list-group">
+			    <a href="" class="list-group-item active"><strong><?= $start_time ?> &ndash; <?= $end_time ?></strong></a>
+			    <?php foreach ($schedule as $event) { event_group_list_item($event, $start_time, $evening_date, $INSTRUCTORS); } ?>
+			</div>
+		    </div>
+		    <?php endforeach ?>
+                </div>
+		<div class="col-xs-12 col-sm-12 col-md-4">
+                    <div class="panel" style="font-size:120%">
+                        <div class="panel-header"><h4>Tonight: 15.03.</h4></div>
+                        <div class="panel-body">
+				<p>16.03. 1:15pm Diversity &amp; Inclusion Task Force in the Europa Saal</p>
+				<p>SC 1 will be in Forum 1 and SC 2 will be in Forum 2</p>
+				<p style="color: red;">Bring your <u>wallet</u> to lunch</p>
+                        </div>
+                    </div>
+                </div>
+		<div class="col-xs-12 col-sm-12 col-md-5">
+                    <div class="panel" style="font-size:120%">
+                        <div class="panel-header"><h4>Announcements</h4></div>
+                        <div class="panel-body">
+                            <p>Do you have images from IK? Please share with Jochen or Michael</p>
+                            <p>Hate paywalls? Paste the URL into <a href="http://sci-hub.io">sci-hub.io</a>.</p>
+                            <p>Please upload your slides here: <a href="http://guenne.ik/incoming">http://guenne.ik/incoming</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
+                <div class="col-xs-12 col-sm-12 col-md-3 panel panel-default" style="padding:0;">
+                        <div class="panel-heading hidden-xs hidden-sm">Shoutbox</div>
+                        <form action="shoutbox.php" method="post" id="shoutboxform">
+                            <input type="text" id="shoutboxmessage" name="msg" accesskey="s" placeholder="message" style="width:100%;">
+                        </form>
+                        <div class="container-fluid" style="height: 36em; overflow-y: auto; padding-left:0; padding-right:0;" id="shoutbox">
+                            <table class="table table-condensed">
+                            <?php foreach ($chat as $line) : list($time, $ip, $msg) = explode(' ', $line, 3) ?>
+                                <tr>
+                                    <td width="20" bgcolor="<?= substr(md5($ip), 0, 6); ?>"></td>
+                                    <td style="word-break:break-all;word-wrap:break-word;"><span class="text-muted"><?= date('d. H:i', $time) ?></span> <?= trim($msg); ?></td>
+                                </tr>
+                            <?php endforeach ?>
+                            </table>
+                        </div>
+                </div>
+
+                <div class="col-md-3 hidden-xs hidden-sm text-center">
+                    <a href="http://guenne.ik/images" class="thumbnail" id="impression">
+                        <img src="<?php echo $path . $img ?>" alt="IK Impression">
+                    </a>
+                </div>
+            </div>
         </div>
+
+        <div class="<?= fluid_if_fullscreen(); ?>">
+            <div class="footer-bottom">
+                    <div class="text-center">
+                        <ul class="list-inline">
+                            <li><a href="http://guenne.ik/slides">http://guenne.ik/slides</a></li>
+                            <li><a href="http://guenne.ik/images">http://guenne.ik/images</a></li>
+                            <li><a href="https://www.facebook.com/groups/270641113015786/?fref=nf">IK Facebook Group</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+	</div>
     </body>
 </html>
