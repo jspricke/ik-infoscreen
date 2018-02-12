4f617fddc267b9f4079b4f2978642e92db7050cf
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2018-02-12 08:56:35 -0800

    Fix updateschedule script for 2018

---
+++ updateschedule.sh
@@ -0,0 +1,1 @@
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "request=see_all&start=2018-03-09&end=2018-03-17" "https://www.interdisciplinary-college.de/?controller=AJAX&action=get_calendar_events" -O
