8add70e38bdec453676218707b6900fcf4b9ca97
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2018-02-14 08:15:55 -0800

    Fix updateschedule

--- updateschedule.sh
+++ updateschedule.sh
@@ -1,1 +1,2 @@
-curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "request=see_all&start=2018-03-09&end=2018-03-17" "https://www.interdisciplinary-college.de/?controller=AJAX&action=get_calendar_events" -O
+#!/bin/sh
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "request=see_all&start=2018-03-09&end=2018-03-17" "https://www.interdisciplinary-college.de/?controller=AJAX&action=get_calendar_events" -o ikschedule.json
