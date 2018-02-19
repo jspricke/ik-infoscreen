94309df18ef1e6a75e51940076dff81bf89e5c22
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2018-02-18 21:45:53 -0800

    Update details.sh

--- details.sh
+++ details.sh
@@ -1,10 +1,12 @@
 #!/bin/sh -e
-wget -rk http://www.interdisciplinary-college.de/ || true
-(cd www.interdisciplinary-college.de
-rename 's/index.html\?controller=collections&action=see_detail&id=(\d*)/detail$1.html/' *
-sed -i -e '1,/col-lg-5">/ c <html><body>' -e '/!-- <div class="col-lg-4">/,$ c </body></html>' detail*
-)
-rm -r details
-mkdir details
-mv www.interdisciplinary-college.de/detail*.html details/
-rm -r www.interdisciplinary-college.de
+mkdir -f details
+cd details
+rm -f ./*
+for id in $(grep -oP '"coll_id":"\K[0-9]+' ../ikschedule.json | uniq); do
+	curl "https://www.interdisciplinary-college.de/index.php?controller=collections&action=see_detail&id=$id" | \
+	sed -e '1,/panel-heading">/ c <html><body>' -e '/!-- <div class="col-lg-4">/,$ c </body></html>' | \
+	grep -Ev "</?div" > "detail$id.html"
+	IMG="$(grep -oP '<img .* src="\K[^"]*' "detail$id.html")"
+	curl -O "$IMG"
+	sed -i -e "s#$IMG#$(basename "$IMG")#" "detail$id.html"
+done
