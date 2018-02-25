3cfd1ef2b4b0865396c7431b40282b8bc4802a99
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2018-02-25 14:26:25 +0100

    Rewrite details.py to use json

--- details.py
+++ details.py
@@ -1,70 +1,130 @@
-#!/usr/bin/env python3
-import json
-import os
+#!/usr/bin/python3
 
-import requests
-from bs4 import BeautifulSoup
-
-
-def download_course(id):
-    url = f'https://www.interdisciplinary-college.de/index.php?controller=collections&action=see_detail&id={id}'
-    print(f'Retrieving {url}.')
-    return requests.get(url).content
+from json import _default_decoder, decoder
+from os import makedirs
+from os.path import basename
+from re import compile, search, sub
+from urllib.parse import urlencode
+from urllib.request import urlopen
 
 
-def parse_and_create_details(download, id):
-    html = BeautifulSoup(download, 'html.parser')
+urls = compile(r'(((https?|ftp|git|file|mailto|doi)[.:][^	 ,"\'<>\){}]*|www\.[-a-z0-9.]+)[^	 .,"\'<>\){}])')
 
-    title = html.find(class_='panel-heading').get_text().strip()
-    desc_body, inst_body = html.find_all(class_='panel-body')
 
-    course_contents = desc_body.find_all('strong')
-    for i, course_content in enumerate(course_contents):
-        cc = [course_content.next_sibling]
-        try:
-            while cc[-1].next_sibling.name != 'strong':
-                cc.append(cc[-1].next_sibling)
-        except AttributeError:
-            pass
-        course_contents[i] = [c for c in cc if c != '\n']
-    description, objectives, literature, location, requirements = [c[0] for c in course_contents]
+def get_json():
+    data = urlencode({'start': '2018-03-09', 'end': '2018-03-17', 'request': 'see_all'}).encode('ascii')
+    return urlopen('https://www.interdisciplinary-college.de/index.php?controller=collections&action=see_detail_from_all_json', data).read().decode('utf-8')
 
-    website, image, vita = [0, 0, 0]
-    instructor = inst_body.div.div.table.tr.td.get_text().strip()
+
+# http://www.benweaver.com/blog/decode-multiple-json-objects-in-python.html
+def iload_json(buff, decoder=None, _w=decoder.WHITESPACE.match):
+    """Generate a sequence of top-level JSON values declared in the
+    buffer.
+
+    >>> list(iload_json('[1, 2] "a" { "c": 3 }'))
+    [[1, 2], u'a', {u'c': 3}]
+    """
+
+    decoder = decoder or _default_decoder
+    idx = _w(buff, 0).end()
+    end = len(buff)
 
     try:
-        website = inst_body.find('a')['href'].strip()
+        while idx != end:
+            (val, idx) = decoder.raw_decode(buff, idx=idx)
+            yield val
+            idx = _w(buff, idx).end()
+    except ValueError as exc:
+        raise ValueError('%s (%r at position %d).' % (exc, buff[idx:], idx))
+
+
+def create_aside(attributes, suffix):
+    firstname_key = f'experimenter_firstname{suffix}'
+    if firstname_key not in attributes:
+        return None, None
+
+    firstname = attributes[firstname_key].strip()
+
+    if not firstname:
+        return None, None
+
+    lastname = attributes[f'experimenter_lastname{suffix}'].strip()
+
+    instructor = f'{firstname} {lastname}'
+
+    title_key = f'experimenter_title{suffix}'
+    if title_key in attributes:
+        title = f'{attributes[title_key]}'.strip()
+        if title:
+            instructor = f'{title} {instructor}'
+
+    aside = []
+    aside.append(f'<h2>{instructor}</h2>')
+
+    email = attributes[f'experimenter_email{suffix}']
+
+    if email and email != 'None':
+        aside.append(f'<p>Email: {email}</p>')
+
+    aff_key = f'experimenter_affiliation{suffix}'
+    if aff_key in attributes:
+        affiliation = attributes[aff_key]
+        if affiliation:
+            aside.append(f'<p>Affiliation: {affiliation}</p>')
+
+    website = attributes[f'experimenter_website{suffix}']
+    if website and website != 'TBA' and website != 'None':
         websites = website.split('<br />')
         websites = [f'<a href="{website}">{website}</a>'for website in websites]
         website = '<br />'.join(websites)
-    except TypeError:
-        website = 'N/A'
+        aside.append(f'<p>Website: {website}</p>')
 
-    try:
-        image = inst_body.find('img')['src'].strip()
-        imagetag = f'<img src="{id}{os.path.splitext(image)[1]}" alt="{instructor}" />'
-    except TypeError:
-        image = None
-        imagetag = ''
+    vita = attributes[f'experimenter_vita{suffix}']
+    vita = sub(urls, r'<a href="\1">\1</a>', vita)
 
-    vita = inst_body.find_all('strong')[1].next_sibling.next_sibling.children
-    vita = ''.join(str(v) for v in vita)
+    pic_key = f'experimenter_pic{suffix}'
+    image = None
+    if pic_key in attributes:
+        image_url = search(r'src="([^"]*)"', attributes[pic_key])
+        if image_url:
+            image = image_url.group(1)
+            aside.append(f'<p><img src="{basename(image)}" alt="{instructor}" /> {vita}</p>')
+    else:
+        aside.append(f'<p>{vita}</p>')
+
+    return '\n'.join(aside), image
+
+
+def create_details(json):
+    asides = []
+    images = []
+    for suffix in ('', '_2', '_3'):
+        aside, image = create_aside(json['attributes'], suffix)
+        if aside:
+            asides.append(aside)
+        if image:
+            images.append(image)
+
+    asides = "\n".join(asides)
+    literature = sub(urls, r'<a href="\1">\1</a>', json['attributes']['literature'])
 
     details = f'''<!DOCTYPE html>
 <head>
-    <title>{title}</title>
+    <meta charset="utf-8">
+    <title>{json['title'].replace('<b>', '').replace('</b>', '')}</title>
     <link rel="stylesheet" type="text/css" href="/css/detail.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-    <h1>{title}</h1>
+    <h1>{json['title']}</h1>
     <article>
         <section>
             <h2>Description</h2>
-            {description}
+            {json['attributes']['description']}
         </section>
         <section>
             <h2>Objectives</h2>
-            {objectives}
+            {json['attributes']['objectives']}
         </section>
         <section>
             <h2>Literature</h2>
@@ -72,39 +132,31 @@ def parse_and_create_details(download, id):
         </section>
         <section>
             <h2>Requirements</h2>
-            {requirements}
+            {json['attributes']['course_requirements']}
         </section>
         <section>
             <h2>Room</h2>
-            {location}
+            {json['attributes']['experiment_location']}
         </section>
     </article>
     <aside>
-        <h2>{instructor}</h2>
-        <p>Website: {website}</p>
-        <p>{imagetag}
-        {vita}</p>
+        {asides}
     </aside>
 </body>
 </html>'''
-    return details, image
+    return details, images
 
 
 def main():
-    with open('ikschedule.json') as iksched:
-        schedule = json.load(iksched)
-    ids = sorted(list(set(course['coll_id'] for course in schedule['events'])))
-    os.makedirs('details', exist_ok=True)
-    for id in ids:
-        with open(f'details/detail{id}.html', 'w') as f:
-            output, img = parse_and_create_details(download_course(id), id)
-            f.write(output)
-        if img:
-            print(f'Retrieving {img}.')
-            resp = requests.get(img, stream=True)
-            with open(f'details/{id}{os.path.splitext(img)[1]}', 'wb') as f:
-                for block in resp.iter_content(2**10):
-                    f.write(block)
+    makedirs('details', exist_ok=True)
+
+    for detail in iload_json(get_json()):
+        output, images = create_details(detail['data'])
+        open(f'details/detail{detail["collection_id"]}.html', 'w').write(output)
+
+        for image in images:
+            print(f'Retrieving {image}.')
+            open(f'details/{basename(image)}', 'wb').write(urlopen(image).read())
 
 
 if __name__ == '__main__':
