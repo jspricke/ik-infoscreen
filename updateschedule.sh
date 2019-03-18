0bb79e96459ba00f0787b11d533b4578e8750f79
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2019-03-18 18:04:45 +0100

    Use dirname for update path

--- updateschedule.sh
+++ updateschedule.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "request=see_all&start=2019-03-12&end=2019-03-19" "https://www.interdisciplinary-college.de/?controller=AJAX&action=get_calendar_events" -o /var/www/html/ikschedule.json
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "request=see_all&start=2019-03-12&end=2019-03-19" "https://www.interdisciplinary-college.de/?controller=AJAX&action=get_calendar_events" -o "$(dirname "$0")/ikschedule.json"
