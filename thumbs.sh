287f927b132a705d78bcde96d7af6c0b022a0f32
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2019-03-24 13:24:20 +0100

    Add svg version of the map

--- thumbs.sh
+++ thumbs.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -e
-rm thumbs/*
+rm -rf thumbs
+mkdir thumbs
 for i in images/*/*; do convert "$i" -resize 300 thumbs/"$(basename "$i")"; done
 chmod a+r thumbs/*
-cp images/ik/guenne.png thumbs/guenne.png
+cp images/ik/guenne.svg thumbs/guenne.svg
