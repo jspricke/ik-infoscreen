7c81d0c37d3e82caf4b4def56c4e41556c381bc2
v4hn <me@v4hn.de>
2019-03-31 20:15:01 +0200

    Skip videos when generating thumbs

--- thumbs.sh
+++ thumbs.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 rm -rf thumbs
 mkdir thumbs
-for i in images/*/*; do convert "$i" -resize 300 thumbs/"$(basename "$i")"; done
+for i in images/*/*; do
+	[[ $i = *.mp4 ]] && continue
+	[[ $i = *.MOV ]] && continue
+	convert "$i" -resize 300 thumbs/"$(basename "$i")"
+done
 cp images/ik/guenne.svg thumbs/guenne.svg
 chmod a+r thumbs/*
