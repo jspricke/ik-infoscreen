5a916a3c39259eac3ffd1d0a3e0b48ca5bc09e67
v4hn <me@v4hn.de>
2019-03-31 20:12:20 +0200

    Generate individual slide zips

--- slides.sh
+++ slides.sh
@@ -1,6 +1,20 @@
 #!/bin/sh -e
 
-rm -f slides/slides.zip
-zip -r slides.zip slides/
+FILENAME=slides/all_slides.zip
+
+zip -r slides.zip slides/ \
+    -x "slides/*.zip" \
+       "slides/zip/*" \
+       "slides/MISSING_SLIDES"
 chmod 0666 slides.zip
-mv slides.zip slides/
+mv slides.zip $FILENAME
+
+# prepare individual zips
+cd slides
+mkdir -p zip
+rm -f zip/*
+ls | while read course; do
+	if [ -d "$course" ] || [ "$course" = "zip" ]; then # todo: the latter action fails
+		zip -r "zip/$course.zip" "$course"
+	fi
+done
