d8ab5694105b1ff8fa9dcb609c59bbcc5e1472c9
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2019-05-19 17:11:14 +0200

    Fix bashism

--- thumbs.sh
+++ thumbs.sh
@@ -2,9 +2,11 @@
 rm -rf thumbs
 mkdir thumbs
 for i in images/*/*; do
-	[[ $i = *.mp4 ]] && continue
-	[[ $i = *.MOV ]] && continue
-	convert "$i" -resize 300 thumbs/"$(basename "$i")"
+	case "$(file -b "$i")" in
+		*"image data"*)
+			convert "$i" -resize 300 thumbs/"$(basename "$i")"
+			;;
+	esac
 done
 cp images/ik/guenne.svg thumbs/guenne.svg
 chmod a+r thumbs/*
